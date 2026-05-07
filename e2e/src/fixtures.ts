1849d202b060c3e6c56bae69040966d4086e53a4
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 14:17:49 -0600

    Remove unused fixtures from e2e tests
    Only openRouterToolkitPage is used in the test suite. Removed AppCatalogPage, appName, and config fixtures that were defined but never destructured.

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,27 +1,13 @@
 import { test as baseTest } from '@playwright/test';
-import {
-  AppCatalogPage,
-  config,
-} from '@crowdstrike/foundry-playwright';
 import { OpenRouterToolkitPage } from './pages/OpenRouterToolkitPage';
 
 type FoundryFixtures = {
-  appCatalogPage: AppCatalogPage;
   openRouterToolkitPage: OpenRouterToolkitPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
   openRouterToolkitPage: async ({ page }, use) => {
     await use(new OpenRouterToolkitPage(page));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
   },
 });
 
